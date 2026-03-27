5a09c14df8ea4ddd961b65c13c4c289cb5903b69
Wuttipongs232 <684259055@webmail.npru.ac.th>
2026-03-27 12:24:20 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0, sum = 0, e = 0, o = 0, max = -999999, min = 999999;
+        System.out.println("ใส่เลขไรก็ใส่จะหยุดใส่-1ให้มันหยุด");
+
+        while (true) {
+            int num = sc.nextInt();
+            if (num == -1)
+                break;
+            count++;
+            sum += num;
+            if (num > max)
+                max = num;
+            if (num < min)
+                min = num;
+        }
+        if (count == 0) {
+
+            System.out.println("ไม่มีข้อมูล");
+        } else {
+            double avg = (double) sum / count;
+            System.out.println("จำนวน: " + count);
+            System.out.println("รวม: " + sum);
+            System.out.println("ค่าเฉลี่ย: " + avg);
+            System.out.println("สูงสุด: " + max);
+            System.out.println("ต่ำสุด: " + min);
+        }
+    }
+}
